1f444b39082bda26c43a85a6a963e2a688667d24
bhuvaneshgujarkar <118657545+bhuvaneshgujarkar@users.noreply.github.com>
2025-01-27 15:14:42 +0530

    add

---
+++ first program.cpp
@@ -0,0 +1,41 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"hello world"<<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 123;
+//     cout<< a <<endl;
+
+//     char c = 'b';
+//     cout<< c <<endl;
+
+//     bool b1= true;
+//     cout<<b1<<endl;
+
+//     float f=1.2;
+//     cout<<f<<endl;
+
+//     double d=1.23;
+//     cout<<d<<endl;
+
+//     int size=sizeof(d);
+//     cout<<"Size of d is: "<<size<<endl;
+
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 3.0/4;
+    cout<< a << endl;
+    cout<< 3.0/4 <<endl;
+}
