846f6bb42f91bdb7c85d1b4511adf4724ad05e73
bhuvaneshgujarkar <118657545+bhuvaneshgujarkar@users.noreply.github.com>
2025-01-27 15:40:38 +0530

    add

---
+++ .vscode/codes/2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<"the value of n is: "<<a<<endl;
+    if(a>0)
+    {
+        cout<<"A is positive"<<endl;
+    }
+}
